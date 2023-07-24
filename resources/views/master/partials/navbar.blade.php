8c78644cc488272199f77bf6eb5459b5e3dcd38c
fendishatech <liyukibir@gmail.com>
2023-07-24 06:08:56 -0700

    migrations, models and controllers

--- resources/views/master/partials/navbar.blade.php
+++ resources/views/master/partials/navbar.blade.php
@@ -1,21 +1,23 @@
-<header class="text-gray-600 bg-gray-200 body-font">
+<header class="text-yellow-700 bg-gray-200 body-font">
     <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a class="hidden md:flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round"
-                stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
-                viewBox="0 0 24 24">
-                <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-            </svg>
-            <span class="ml-3 text-xl">Header Logo</span>
-        </a>
+        <div class="flex items-center border-b border-green-500 py-2">
+            <input
+                class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+                type="text" placeholder="Search">
+            <button
+                class="flex-shrink-0 bg-green-500 hover:bg-green-700 border-green-500 hover:border-green-700 text-sm border-4 text-white py-1 px-2 rounded"
+                type="button">
+                <i class="fa fa-search" aria-hidden="true"></i>
+            </button>
+        </div>
         <nav class="hidden md:flex md:ml-auto flex-wrap items-center text-base justify-center">
-            <a class="mr-5 hover:text-gray-900">First Link</a>
-            <a class="mr-5 hover:text-gray-900">Second Link</a>
+            <a class="mr-5 hover:text-green-900 cursor-pointer">First Link</a>
+            <a class="mr-5 hover:text-green-900 cursor-pointer">Second Link</a>
         </nav>
         <form action="{{ url('/logout') }}" method="post">
             @csrf
-            <input class="bg-green-600 px-6 py-2 rounded text-white font-semibold cursor-pointer" type="submit"
-                value="Logout" />
+            <input class="bg-green-600 hover:bg-green-800 px-6 py-2 rounded text-white font-semibold cursor-pointer"
+                type="submit" value="Logout" />
         </form>
     </div>
 </header>
