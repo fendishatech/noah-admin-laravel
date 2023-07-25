82dff12b8713dc9466d52aa6736948f11877106e
fendishatech <liyukibir@gmail.com>
2023-07-25 05:16:46 -0700

    side menu and lot model

---
+++ app/Http/Controllers/MemberLotsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MemberLots;
+use Illuminate\Http\Request;
+
+class MemberLotsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(MemberLots $memberLots)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(MemberLots $memberLots)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, MemberLots $memberLots)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(MemberLots $memberLots)
+    {
+        //
+    }
+}
--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,5 +1,6 @@
 @php
-    $menuItems = [['url' => '/home', 'label' => 'Home', 'icon' => 'fa-home'], ['url' => '/users', 'label' => 'Users', 'icon' => 'fa-user-circle'], ['url' => '/clients', 'label' => 'Clients', 'icon' => 'fa-users'], ['url' => '/members', 'label' => 'Members', 'icon' => 'fa-id-badge']];
+    $menuItems = [['url' => '/home', 'label' => 'Home', 'icon' => 'fa-home'], ['url' => '/users', 'label' => 'Users', 'icon' => 'fa-user-circle'], ['url' => '/clients', 'label' => 'Clients', 'icon' => 'fa-users'], ['url' => '/cities', 'label' => 'Cities', 'icon' => 'fa-id-badge'], ['url' => '/sub_cities', 'label' => 'SubCities', 'icon' => 'fa-id-badge']];
+    $memberMenuItems = [['url' => '/members', 'label' => 'List', 'icon' => 'fa-list'], ['url' => '/member_ids', 'label' => 'Ids', 'icon' => 'fa-id-card'], ['url' => '/member_lots', 'label' => 'Lots', 'icon' => 'fa-briefcase'], ['url' => '/member_jobs', 'label' => 'Jobs', 'icon' => 'fa-briefcase'], ['url' => '/member_addresses', 'label' => 'Addresses', 'icon' => 'fa-id-badge'], ['url' => '/member_ec', 'label' => 'Em Contacts', 'icon' => 'fa-id-badge'], ['url' => '/member_edus', 'label' => 'Educations', 'icon' => 'fa-id-badge']];
 @endphp
 
 
@@ -7,6 +8,16 @@
     <h2 class="text-white px-6 py-5 bg-green-600 font-bold mb-4">{{ $title }}</h2>
     <ul>
         @foreach ($menuItems as $item)
+            <li
+                class="py-2 flex items-center justify-start px-6 text-yellow-800 hover:text-yellow-600 hover:bg-gray-100 hover:rounded-l-[18px]">
+                <a class="w-full" href="{{ $item['url'] }}"><i class="fa {{ $item['icon'] }} mr-3"
+                        aria-hidden="true"></i><span class="hidden md:inline-flex">{{ $item['label'] }}</span></a>
+            </li>
+        @endforeach
+        <i class="fa fa-address-book" aria-hidden="true"></i>
+        con
+        <h2 class="px-6 py-2 text-yellow-600 font-semibold">Members</h2>
+        @foreach ($memberMenuItems as $item)
             <li
                 class="py-2 flex items-center justify-start px-6 text-yellow-800 hover:text-yellow-600 hover:bg-gray-100 hover:rounded-l-[18px]">
                 <a class="w-full" href="{{ $item['url'] }}"><i class="fa {{ $item['icon'] }} mr-3"
